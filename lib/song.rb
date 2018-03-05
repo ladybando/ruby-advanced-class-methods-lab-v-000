8732a69a93787e3d45b528d93bd2d78aa6f0c12a
Ayana Bando <ayanaflax@yahoo.com>
2018-03-05 17:36:27 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -37,4 +37,12 @@ class Song
     song.save
     song
   end
-end
+
+  def self.find_by_name(name)
+    @@all.detect{|song|
+    song.name = name}
+  end
+
+  def self.find_or_create_by_name(name)
+    
+end 
