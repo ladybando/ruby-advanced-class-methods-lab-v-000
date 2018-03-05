cd431140159c51d087fd1f6e5107af1b5050e3e6
Ayana Bando <ayanaflax@yahoo.com>
2018-03-05 17:30:48 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,6 +17,13 @@ class Song
     song
   end
 
+  def self.create_by_name(name)
+    song= create.new
+    song.name = name
+    song.save
+    song
+  end
+  
   def self.new_by_name(name)
     song=self.new
     song.name= name
