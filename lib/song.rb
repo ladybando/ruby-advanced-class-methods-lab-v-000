6b6823feb3899610afaebe9ff862875f5b986f2e
Ayana Bando <ayanaflax@yahoo.com>
2018-03-05 18:09:22 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -47,7 +47,7 @@ class Song
     end
 
   def self.alphabetical
-   @@all.sort_by {|song| song.name == name}
+   self.all.sort_by {|song| song.name == name}
   end
 
   def self.new_from_filename(file)
